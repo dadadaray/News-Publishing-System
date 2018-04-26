2a7c9c38740242f8dfa3aa276c4714f5164358bc
LuHoney7 <441007713@qq.com>
2018-04-26 20:55:28 +0800

    后台草稿箱和用户管理

--- News-Publishing-System/src/com/aps/backstage/loginUser/dao/BackUserDaoImpl.java
+++ News-Publishing-System/src/com/aps/backstage/loginUser/dao/BackUserDaoImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.aps.entity.LoginUser;
 import com.framework.BaseDao;
+import com.framework.Page;
 
 @Repository
 public class BackUserDaoImpl extends BaseDao<LoginUser, String> {
@@ -27,6 +28,22 @@ public class BackUserDaoImpl extends BaseDao<LoginUser, String> {
 			return (LoginUser) query.uniqueResult();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public Page<LoginUser> reporterList(int pageNum, int pageSize, Object[] params){
+		String hql;
+		hql="from LoginUser u where n.userInfo.userInfoId = ? and n.statues = 1 order by n.createTime desc";
+		params[0]=params[0];
+		try{
+			Page<LoginUser> page = new Page<LoginUser>();
+			page.setCurrentPageNum(pageNum);
+			page.setPageSize(pageSize);
+			page = this.findByPage(pageNum, pageSize, hql, params);
+			return page;
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
