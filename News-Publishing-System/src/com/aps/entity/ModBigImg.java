286ffeaca153b51dba3a48b4443f98147e26bbaa
DESKTOP-SO7BN73\Ray <yanrongen@gmail.com>
2018-04-29 15:55:09 +0800

    删除多余代码

--- News-Publishing-System/src/com/aps/entity/ModBigImg.java
+++ News-Publishing-System/src/com/aps/entity/ModBigImg.java
@@ -1,13 +1,11 @@
 package com.aps.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
