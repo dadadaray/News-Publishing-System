2b19e9a34531056472b6a2e33d744240fbbc60da
DESKTOP-SO7BN73\Ray <yanrongen@gmail.com>
2018-05-08 18:21:15 +0800

    删除用户的所有的评论

--- News-Publishing-System/src/com/aps/comment/dao/CommentDaoImpl.java
+++ News-Publishing-System/src/com/aps/comment/dao/CommentDaoImpl.java
@@ -1,5 +1,8 @@
 package com.aps.comment.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.hibernate.Query;
@@ -32,19 +35,28 @@ public class CommentDaoImpl extends BaseDao<Comment, String> {
 		Query querys = this.sessionFactory.getCurrentSession().createQuery(
 				"from Comment n where n.userInfo.userInfoId in" + SqlUtils.toLikeSqlForStr(userInfoIds, ","));
 		// querys.list();
+		List<Comment> list  = new ArrayList<Comment>(0);
+		list=querys.list();
+		
 		//先删除子评论
 		for (int i = 0; i < querys.list().size(); i++) {
-			Comment comment = (Comment) querys.list().get(i);
-			if (comment.getParentComment() != null) {
-				deleteComment(comment.getCommentId());
+			Comment comments = (Comment) querys.list().get(i);
+			if (comments.getParentComment() != null) {
+				//System.out.print(comments.getCommentContent());
+				deleteComment(comments.getCommentId());
 			}
 		}
 		//再删除所有评论
 		Query query = this.sessionFactory.getCurrentSession().createQuery(
 				"from Comment n where n.userInfo.userInfoId in" + SqlUtils.toLikeSqlForStr(userInfoIds, ","));
 		// query.list();
-		for (int i = 0; i < query.list().size(); i++) {
+		List<Comment> list1  = new ArrayList<Comment>(0);
+		list1=query.list();
+	    int len=list1.size();
+		//System.out.print("开始删父评论");
+		for (int i = 0; i < query.list().size();i=0) {
 			Comment comment = (Comment) query.list().get(i);
+			//System.out.print(comment.getCommentContent());
 			deleteComment(comment.getCommentId());
 		}
 		return ret;
